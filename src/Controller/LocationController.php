2a7f8acbf9e36c99754e5497dfaefb4cf513e7db
mrichou2023 <morgane.richou@gmail.com>
2023-05-31 11:11:11 +0200

    [ADD]added location Add

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Location;
+use App\Form\LocationType;
 use App\Repository\LocationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/location', name:'location_')]
 class LocationController extends AbstractController
 {
     #[Route('/add', name:'add')]
@@ -23,6 +25,6 @@ class LocationController extends AbstractController
             $this->addFlash('success','Location successfully added !');
             return $this->redirectToRoute('event_add');
         }
-        return $this->render('location/add.html.twig', ['eventForm' => $locationForm->createView()]);
+        return $this->render('location/add.html.twig', ['locationForm' => $locationForm->createView()]);
     }
 }
