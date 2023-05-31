3c3eec5e58114eec18b19aca2b60081b9eb42088
TanguiLouedec <tangui.louedec@gmail.com>
2023-05-31 11:17:48 +0200

    [WIP] Registration and Profile

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,9 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\User;
+use App\Repository\CampusRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,6 +21,13 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationFormType extends AbstractType
 {
+
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -24,6 +37,9 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('firstname')
             ->add('lastname')
+            ->add('campus', ChoiceType::class, [
+                'choices' => $this->getCampus()
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -44,6 +60,17 @@ class RegistrationFormType extends AbstractType
         ;
     }
 
+    private function getCampus()
+    {
+        $repository = $this->entityManager->getRepository(Campus::class);
+        $campus = $repository->findAll();
+        $choices = array_reduce($campus, function ($result, $campus) {
+            $result[$campus->getName()] = $campus->getId();
+            return $result;
+        }, []);
+        //dd($choices);
+        return $choices;
+    }
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
