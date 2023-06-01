669bb81cd95f876c6e9489ac0c7be4b55023188c
TanguiLouedec <tangui.louedec@gmail.com>
2023-06-01 14:42:06 +0200

    [ADD] Tailwind config working

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,9 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 Encore
     .enablePostCssLoader()
     // directory where compiled assets will be stored
-    .setOutputPath('public/build/website/')
+    .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build/website')
+    .setPublicPath('/ENIvent/public/build')
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
